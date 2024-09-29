0c50d58cf68f85255d4945b6f31cea27d4a4d93a
Erik Brück <109608462+ErikBruck@users.noreply.github.com>
2024-09-29 23:25:07 +0300

    feat(counter): add Counter component and update app js and esimeneKomponent js

--- src/Komponendid/Counter.js
+++ src/Komponendid/Counter.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 
 const Counter = () => {
-    const [count, setCount] = useState(0); // Initialize state variable 'count'
+    const [count, setCount] = useState(0);
 
-    // Function to change the counter value
+
     const changeCounter = (change) => {
-        setCount(count + change); // Update the count by adding the 'change' value
+        setCount(count + change);
     };
 
     return (
         <div>
             <h1>Count: {count}</h1>
-            {/* Buttons to modify the counter */}
             <button onClick={() => changeCounter(-1)}>Decrement by 1</button>
             <button onClick={() => changeCounter(1)}>Increment by 1</button>
             <button onClick={() => changeCounter(-count)}>Reset</button>
