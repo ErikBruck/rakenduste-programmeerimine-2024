a2b8bfc0dc781bba5467895d8e364f410bf680fa
Erik Brück <109608462+ErikBruck@users.noreply.github.com>
2024-09-30 01:43:15 +0300

    organize files into folders

---
+++ second/second.js
@@ -0,0 +1,25 @@
+function addNumbersFn(num1, num2) {
+    return num1 + num2
+}
+
+console.log(addNumbersFn(1, 2));
+// 3 ylesanne ymber kirjutada arrow funktsiooni
+
+const addNumbersFn = (num1, num2) => {
+    return num1 + num2
+};
+
+console.log(addNumbersFn(1, 2));
+
+// 4 ylesanne ymber kirjutada aroow funktsiooniks
+
+//nested
+
+//5 ylesanne luua funktsioon sellisel kujul, et saaks kutsuda välja seda nii:
+
+console.log(addNumbersNested(3)(4));
+
+//6 ylesanne looge samast funktsioonist arrowfunktsioon
+
+//7 ylesanne AF printida "Hello (nimi)" ja kasutada string literals
+
